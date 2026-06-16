b5069a57755593b8f3b5f1073946f1f40afef219
AAEHU <aavehu@elevateblackvoiceshu.com>
2026-06-16 13:53:48 -0400

    Refactor logging in monitor.py and update output file handling; remove obsolete result files and add new time-split results

--- monitor.py
+++ monitor.py
@@ -5,12 +5,14 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--filename", default=f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt", help="Log file path")
 args = parser.parse_args()
+output_file = args.filename
 
 # Define your target string
 TARGET_STRING1 = "fit_file starting — MC first, then CNMF init"
 TARGET_STRING2 = "fit_file done"
+TARGET_STRING3 = "Time split:"
 start_time = datetime.now().time()
-with open(f"logs/{args.filename}", "a") as f:
+with open(f"logs/{output_file}", "a") as f:
     f.write(f"Monitoring started at {start_time}\n")
 
 # Read line-by-line from the terminal stream
@@ -21,11 +23,15 @@ for line in sys.stdin:
 
     # Trigger condition
     if TARGET_STRING1 in line:
-        with open(f"logs/{args.filename}", "a") as f:
+        with open(f"logs/{output_file}", "a") as f:
             f.write(f"Alert! Found {TARGET_STRING1} in line: {line} at time {datetime.now().time()}\n")
     elif TARGET_STRING2 in line:
-        with open(f"logs/{args.filename}", "a") as f:
+        with open(f"logs/{output_file}", "a") as f:
             f.write(f"Alert! Found {TARGET_STRING2} in line: {line} at time {datetime.now().time()}\n")
+    elif TARGET_STRING3 in line:
+        with open(f"logs/{output_file}", "a") as f:
+            f.write(f"Alert! Found {TARGET_STRING2} in line: {line} at time {datetime.now().time()}\n")
+    
 
-with open(f"logs/{args.filename}", "a") as f:
+with open(f"logs/{output_file}", "a") as f:
     f.write(f"Monitoring ended at {datetime.now().time()}\n")
